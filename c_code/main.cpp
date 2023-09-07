178ae41d4dfbd90726921305137ee35c93345631
wyatt <2671199174@qq.com>
2023-09-07 22:17:07 +0800

    third try

--- c_code/main.cpp
+++ c_code/main.cpp
@@ -4,5 +4,9 @@ int main ()
     //给定一些数据，排列其大小。
     int a1, a2, a3, a4, a5, a6;
     scanf("d% d% d% d% d% d%", &a1, &a2, &a3, &a4, &a5, &a6);
+    if (a1>a2)
+    {
+        printf("d%",a1);
+    }
     return 0;
 }
