0d93d36f9b532c6f22ef892261db4ccd39526893
wyatt <2671199174@qq.com>
2023-09-07 22:07:07 +0800

    second try

--- c_code/main.cpp
+++ c_code/main.cpp
@@ -1,6 +1,8 @@
 #include <cstdio>
 int main ()
 {
-    printf("dhajd");
+    //给定一些数据，排列其大小。
+    int a1, a2, a3, a4, a5, a6;
+    scanf("d% d% d% d% d% d%", &a1, &a2, &a3, &a4, &a5, &a6);
     return 0;
 }
