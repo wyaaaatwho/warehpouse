33f4c3e95d3956656fa7e46e7d351f1b971f1203
wyatt <2671199174@qq.com>
2023-10-16 21:01:09 +0800

    第三次oj更新

--- c_code/days.cpp
+++
@@ -1,6 +0,0 @@
-
-char month [10];
-char weekday [10];
-int day,year,hour,minute,second;
-scanf("%s%d%d%s%d%d%d",&month,&day,&year,&weekday,&hour,&minute,&second);
-printf("%c%c%c %c%c%c %02d %02d:%02d:%02d %04d",weekday[0],weekday[1],weekday[2],month[0],month[1],month[2],day,hour,minute,second,year);
--- c_code/julian.cpp
+++
@@ -1,7 +0,0 @@
-int a,y,m,Jun,day,year,month;
-scanf("%d %d %d",&year,&month,&day);
-a=(14-month)/12;
-y=year+4800-a;
-m=month+12*a-3;
-Jun=day+((153*m+2)/5)+365*y+y/4-y/100+y/400-32045;
-printf("%d",Jun);
