b0f95c9c104c908b7d75efe721c95ade8e4952e2
wyaaaatwho <144138456+wyaaaatwho@users.noreply.github.com>
2023-10-16 21:02:58 +0800

    Update bubble.c.cpp

--- c_code/bubble.c.cpp
+++ c_code/bubble.c.cpp
@@ -1,3 +1,33 @@
 //
 // Created by 26711 on 2023/10/15.
 //
+int n;
+scanf("%d", &n);
+
+for (int i = 0; i < n; i++)
+{
+int len, tf = 0;
+scanf("%d", &len);
+int array[len];
+
+for (int j = 0; j < len; j++)
+{
+scanf("%d", &array[j]);
+}
+
+for (int j = 0; j < len - 1; j++)
+{
+if (array[j] > array[len - 1])
+{
+tf = 1;
+break;
+}
+}
+if (tf)
+{
+printf("No\n");
+} else
+{
+printf("Yes\n");
+}
+}
